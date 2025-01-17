ef23cb4d7b1ad232c77b56263512c96deb917680
Mykhailo Dzhezhelo <m.dzhezhelo@cleack.com>
2025-01-17 02:01:02 +0200

    add flatlist to SectionProductsBySlug

--- source/screen/pages/Home/component/SectionProductList.tsx
+++ source/screen/pages/Home/component/SectionProductList.tsx
@@ -1,25 +1,12 @@
-import { FC, memo, useCallback } from "react";
-import { FlatList, ListRenderItemInfo, StyleSheet, View } from "react-native";
+import { useCallback } from "react";
+import { FlatList, ListRenderItemInfo, StyleSheet } from "react-native";
 
 import { useSelector } from "react-redux";
 
-import { AppText, ListEmptyLoader, ProductCard } from "~/component";
-import { colors, Fonts } from "~/constants";
-import { PRODUCTS_SLUG } from "~/constants/product";
+import { ListEmptyLoader } from "~/component";
 import { IProductBySlug, productsBySlugSelector } from "~/store/product";
 
-const Section: FC<IProductBySlug> = memo(({ slug, products }) => {
-  return slug ? (
-    <View>
-      <AppText styleText={styles.sectionTitle}>{PRODUCTS_SLUG[slug]}</AppText>
-      <View style={styles.columnWrapper}>
-        {products?.map((item) => (
-          <ProductCard {...item} key={item.id} />
-        ))}
-      </View>
-    </View>
-  ) : null;
-});
+import { SectionProductsBySlug } from "./SectionProductsBySlug";
 
 const keyExtractor = (_: IProductBySlug, index: number) => `section-${index}`;
 
@@ -28,7 +15,7 @@ export const SectionProductList = () => {
 
   const renderSection = useCallback(
     ({ item }: ListRenderItemInfo<IProductBySlug>) => {
-      return <Section {...item} />;
+      return <SectionProductsBySlug {...item} />;
     },
     []
   );
@@ -46,24 +33,10 @@ export const SectionProductList = () => {
 };
 
 const styles = StyleSheet.create({
-  columnWrapper: {
-    justifyContent: "space-between",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    rowGap: 22,
-  },
   contentContainer: {
     flexGrow: 1,
-    gap: 20,
+    rowGap: 20,
     paddingHorizontal: 30,
     paddingBottom: 20,
-  },
-  sectionTitle: {
-    fontSize: 17,
-    fontWeight: 600,
-    lineHeight: 21,
-    fontFamily: Fonts.MontserratSemiBold,
-    color: colors.cyanBlue,
-    marginBottom: 22,
   },
 });
